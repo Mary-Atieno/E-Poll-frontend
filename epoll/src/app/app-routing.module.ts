6f22c0eed443fcc4295a64dba9ed7bea33dbb682
FloWambui <gflorencewambui@gmail.com>
2022-07-06 21:56:52 +0300

    add the admin auth routing

--- epoll/src/app/app-routing.module.ts
+++ epoll/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: LandingpageComponent},
+  { path: 'admin-login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
